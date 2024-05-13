06a84d5b5ef2236ecd52e782f10cc1546b1ed2ca
karineaiki <ruiz.karine@gmail.com>
2024-05-13 16:40:54 +0200

    hello world app springboot

---
+++ src/main/java/com/springboot/exo/ExoApplication.java
@@ -0,0 +1,19 @@
+package com.springboot.exo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world");
+	}
+
+}
